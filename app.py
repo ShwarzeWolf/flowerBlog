a2bd1f3811d68b30ce8981e84f1066887aa4d7ea
Kristina Sharaeva <224787@niuitmo.ru>
2021-12-18 18:27:42 +0300

    now posts could be changed

--- app.py
+++ app.py
@@ -59,6 +59,27 @@ def create():
 
     return render_template('create.html')
 
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('draw_main_page'))
+
+    return render_template('edit.html', post=post)
+
 
 if __name__ == '__main__':
     app.run()
