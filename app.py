5a886564ea1357262461e861af4eef34d7b6a2a4
Kristina Sharaeva <224787@niuitmo.ru>
2022-01-29 19:25:21 +0300

    database functions added to separate file

--- app.py
+++ app.py
@@ -1,54 +1,7 @@
-import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
-
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
-
-def get_post(post_id):
-    conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
-    conn.close()
-    return post
-
-
-def get_all_posts():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
-
-    return posts
-
-
-def add_post(title, content):
-    conn = get_db_connection()
-    conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                 (title, content))
-    conn.commit()
-    conn.close()
-
-
-def update_post(title, content, id):
-    conn = get_db_connection()
-    conn.execute('UPDATE posts SET title = ?, content = ?'
-                 ' WHERE id = ?',
-                 (title, content, id))
-    conn.commit()
-    conn.close()
-
-
-def delete_post(id):
-    conn = get_db_connection()
-    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
-    conn.commit()
-    conn.close()
-
-
+from repositories import get_all_posts, get_post, add_post, update_post, delete_post
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
