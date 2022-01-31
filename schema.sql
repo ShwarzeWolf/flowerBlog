52691af1ccdfc1e4a13041fcb32cefe0b996c115
Kristina Sharaeva <224787@niuitmo.ru>
2022-01-31 18:25:38 +0300

    patch

--- schema.sql
+++ schema.sql
@@ -6,3 +6,12 @@ CREATE TABLE posts (
     title TEXT NOT NULL,
     content TEXT NOT NULL
 )
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL
+)
